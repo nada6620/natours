0163cb7ccb0b28fb09ab6df91b4ac0ce5489573c
Nada Ayman <nadaayman0662@gmail.com>
2026-08-07 02:30:25 +0300

    edit base.pug

--- public/js/index.js
+++ public/js/index.js
@@ -78,4 +78,6 @@ if(logoutButton) logoutButton.addEventListener('click',logout)
   })
 
   const alertMessage = document.querySelector('body').dataset.alert
+  console.log('alertMessage =', alertMessage);
+  console.log('typeof =', typeof alertMessage);
   if(alertMessage) showAlert('success',alertMessage,20) 
